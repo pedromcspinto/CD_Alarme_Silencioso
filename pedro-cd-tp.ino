1769f09beb0fb99e8afcee4fb9722618018b2b87
pedromcspinto <38633293+pedromcspinto@users.noreply.github.com>
2023-06-14 04:34:11 +0100

    Update pedro-cd-tp.ino

--- pedro-cd-tp.ino
+++ pedro-cd-tp.ino
@@ -8,28 +8,28 @@
 #define trigger_pin 5
 #define Echo_pin 18
 
+//Ligacao a internet
 const char* ssid = "freeismat";
 const char* password = "";
 
+//numero de telemovel e numero indentificador do mesmo na API
 String phoneNumber = "";
 String apiKey = "";
 
-/* two variables to store duraion and distance value */
 long duration;
 int distance;
 
 void getDistance(){
-  digitalWrite(trigger_pin, LOW); //set trigger signal low for 2us
+  digitalWrite(trigger_pin, LOW);
 delayMicroseconds(2);
 
-/*send 10 microsecond pulse to trigger pin of HC-SR04 */
-digitalWrite(trigger_pin, HIGH);  // make trigger pin active high
-delayMicroseconds(10);            // wait for 10 microseconds
-digitalWrite(trigger_pin, LOW);   // make trigger pin active low
+digitalWrite(trigger_pin, HIGH);  
+delayMicroseconds(10);            
+digitalWrite(trigger_pin, LOW);  
 
-/*Measure the Echo output signal duration or pulss width */
-duration = pulseIn(Echo_pin, HIGH); // save time duration value in "duration variable
-distance= duration*0.034/2; //Convert pulse duration into distance
+
+duration = pulseIn(Echo_pin, HIGH); 
+distance= duration*0.034/2;
 
 Serial.print("Distance: ");
 Serial.print(distance);
@@ -39,15 +39,12 @@ delay(1000);
 
 void sendMessage(String message){
 
-  // Data to send with HTTP POST
   String url = "https://api.callmebot.com/whatsapp.php?phone=" + phoneNumber + "&apikey=" + apiKey + "&text=" + urlEncode(message);    
   HTTPClient http;
   http.begin(url);
 
-  // Specify content-type header
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-  
-  // Send HTTP POST request
+
   int httpResponseCode = http.POST(url);
   if (httpResponseCode == 200){
     Serial.print("Message sent successfully");
@@ -58,15 +55,13 @@ void sendMessage(String message){
     Serial.println(httpResponseCode);
   }
 
-  // Free resources
   http.end();
 }
 
-/* configure D9 and D11 as digital input and output respectively */
 void setup() {
-pinMode(trigger_pin, OUTPUT); // configure the trigger_pin(D9) as an Output
-pinMode(Echo_pin, INPUT); // configure the Echo_pin(D11) as an Input
-Serial.begin(9600); // Enable the serial with 9600 baud rate
+pinMode(trigger_pin, OUTPUT); 
+pinMode(Echo_pin, INPUT); 
+Serial.begin(9600); 
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
   while(WiFi.status() != WL_CONNECTED) {
